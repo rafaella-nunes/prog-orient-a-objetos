25145e28eeb8581283a0fdbd1522b04fde0f44c2
Rafaella <84870082+rafaella-nunes@users.noreply.github.com>
2021-12-16 13:17:24 -0300

    Delete Pessoa directory

--- Pessoa/src/main/java/pessoa/Publicacao.java
+++
@@ -1,9 +0,0 @@
-package pessoa;
-
-public interface Publicacao {
-    public abstract void abrir();
-    public abstract void fechar();
-    public abstract void folhear(int p);
-    public abstract void avancarPag();
-    public abstract void voltarPag();
-}
